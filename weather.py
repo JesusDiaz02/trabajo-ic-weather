6f82d002ddfcf8a151306e64d2d4227da9d234be
jesus diaz <jesusdiazg9@gmail.com>
2022-04-29 09:04:09 -0500

    segundo commit

---
+++ weather.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from warnings import simplefilter
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn import neighbors
+
+simplefilter(action='ignore', category=FutureWarning)
