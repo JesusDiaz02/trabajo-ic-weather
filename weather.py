8ac639d6dd046aada5e6aa844a00d0fe9368da17
jesus diaz <jesusdiazg9@gmail.com>
2022-04-29 09:05:59 -0500

    cuarto commit

--- weather.py
+++ weather.py
@@ -30,3 +30,23 @@ data['Location'].replace(['Canberra','Sydney','Perth','Darwin',
                        18,19,20,21,22,23,24,25,26,27,28,29,30,
                        31,32,33,34,35,36,37,38,39,40,
                        41,42,43,44,45,46,47,48], inplace=True)
+
+                       
+data['WindGustDir'].replace(['W','SE','E','N','SSE','S','WSW','SW',
+                     'SSW','WNW','NW','ENE','ESE','NE','NNW',
+                     'NNE'], [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], inplace=True)
+
+data['WindDir9am'].replace(['W','SE','E','N','SSE','S','WSW','SW',
+                     'SSW','WNW','NW','ENE','ESE','NE','NNW',
+                     'NNE'], [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], inplace=True)
+
+data['WindDir3pm'].replace(['W','SE','E','N','SSE','S','WSW','SW',
+                     'SSW','WNW','NW','ENE','ESE','NE','NNW',
+                     'NNE'], [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], inplace=True)
+
+data.RainToday.replace(['Yes','No'], [1,0], inplace=True)
+
+data.RainTomorrow.replace(['Yes','No'], [1,0], inplace=True)
+
+
+data.dropna(axis=0,how='any',inplace=True) 
