6a015c5d1d196e06169e6782471243512b09a546
jesus diaz <jesusdiazg9@gmail.com>
2022-04-29 09:07:49 -0500

    quinto commit

--- weather.py
+++ weather.py
@@ -50,3 +50,15 @@ data.RainTomorrow.replace(['Yes','No'], [1,0], inplace=True)
 
 
 data.dropna(axis=0,how='any',inplace=True) 
+
+#partir la tabla en dos
+data_train = data[:78001]
+data_test = data[78001:]
+
+x=np.array(data_train.drop(['RainTomorrow'], 1))
+y=np.array(data_train.RainTomorrow)# 0 sale 1 no sale
+
+x_train, x_test, y_train, y_test= train_test_split(x, y, test_size=0.5)
+
+x_test_out = np.array(data_test.drop(['RainTomorrow'], 1))
+y_test_out = np.array(data_test.RainTomorrow)# 0 no llueve 1 llueve
